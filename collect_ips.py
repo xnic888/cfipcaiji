6d71ea2188ce34015fae777a4fa2d50e3847cb1d
xnic888 <112247607+xnic888@users.noreply.github.com>
2024-04-07 16:51:41 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,8 +4,10 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+urls = [
+        'https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
         'https://ip.164746.xyz'
+        'https://stock.hostmonit.com/CloudFlareYes'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -20,7 +22,7 @@ with open('ip.txt', 'w') as file:
     for url in urls:
         # 发送HTTP请求获取网页内容
         response = requests.get(url)
-        
+
         # 使用BeautifulSoup解析HTML
         soup = BeautifulSoup(response.text, 'html.parser')
         
@@ -29,6 +31,23 @@ with open('ip.txt', 'w') as file:
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
+        elif url == 'https://stock.hostmonit.com/CloudFlareYes':
+            response = requests.post('https://api.hostmonit.com/get_optimization_ip', json={"key": "iDetkOys"})
+            if response.status_code == 200:
+                # 解析响应内容为JSON
+                response_json = response.json()
+                
+                # 提取info列表
+                info_list = response_json.get('info', [])
+                
+                # 提取所有IP地址并打印
+                ip_list = [item['ip'] for item in info_list if 'ip' in item]
+                print(ip_list)
+                for ip in ip_list:
+                    file.write(ip + '\n')
+            else:
+                print("Error:", response.status_code)
+            continue
         else:
             elements = soup.find_all('li')
         
